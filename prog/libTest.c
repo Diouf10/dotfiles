5444af58e913fbd34061595a2519ca392d61dc37
Mouhammad Diouf <dioufmouhammad@gmail.com>
2023-09-15 14:59:52 -0400

    Homework overload printf()

---
+++ prog/libTest.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <dlfcn.h>
+
+int printf(const char* name, ...){
+	name = "PAWN: ";
+	return 0;
+}
+
---
+++ prog/test.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+int main(void){
+	printf("Bonjour", 10%d);
+}
